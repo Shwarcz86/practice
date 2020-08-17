9b1c60e6fe2fa2f848f36cc7a5f42c0743062275
Shwarcz86 <69677756+Shwarcz86@users.noreply.github.com>
2020-08-17 13:02:55 +0300

    add files mavic

---
+++ Mavic/js/main.js
@@ -0,0 +1,41 @@
+$(function(){
+
+  $('.products__slider').slick({ // Подключаем слайдер через сборку gulp + jQuery
+    prevArrow: '<button class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>', 
+    //Добавляем кастомные стрелки
+    nextArrow: '<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
+    infinite: false, // Убираем зацикливание после прокрутки с последнего на первый слайд
+  });
+
+  $('.questions__item-title').on('click', function() {
+
+    $('.questions__item').removeClass('questions__item--active'); // У родителей заголовков убирается класс questions__item--active
+    $(this).parent().addClass('questions__item--active'); //Когда кликаем на какой-то конкретный элемент questions__item-title то у родителя добавляется класс 
+  });
+
+/*
+  $('#fullpage').fullpage({
+		// Прокрутка целой страницы
+		autoScrolling:true,
+    scrollHorizontally: true,
+    sectionSelector: '.page-section',
+	});
+*/
+
+});
+
+/*
+let questionsItem = document.querySelectorAll('.questions__item');
+let questionsItemTitle = document.querySelectorAll('.questions__item-title');
+
+for (let i = 0; i < questionsItemTitle.length; i++) {
+  let link = questionsItemTitle[i];
+  link.onclick = function() {
+    for (let j = 0; j < questionsItem.length; j++) {
+      let item = questionsItem[j];
+      item.classList.toggle('questions__item--active');
+      console.log('Привет');
+    }
+  }
+};
+*/
