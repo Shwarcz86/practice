12c5f222786695692d1a0ce17ccf76193bf8fe92
Shwarcz86 <69677756+Shwarcz86@users.noreply.github.com>
2021-06-10 16:45:17 +0300

    Add files via upload

---
+++ Avtogas/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  //MENU
+  $('.header__btn').on('click', function(e) { 
+    $('.header__btn').toggleClass('header__btn_active');
+    $('.header__menu').toggleClass('header__menu_active');
+    $('body').toggleClass('no-scroll');
+  });
+
+  //ТЕЛЕФОН МАСКА
+  $(function() {
+    $("#formPageTel").mask("+7 (999) 999-99-99");
+  });
+  $(function() {
+    $("#formAdvTel").mask("+7 (999) 999-99-99");
+  });
+
+  //ТАБЫ
+  $('.tab').on('click', function(e) { 
+    e.preventDefault();
+
+    $($(this).siblings()).removeClass('tab_active'); 
+    $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content_active');
+
+    $(this).addClass('tab_active'); 
+    $($(this).attr('href')).addClass('tabs-content_active'); 
+  });
+
+
+
+});
