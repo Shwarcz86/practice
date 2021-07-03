a46446d60f6c64be7723a31d69489eab7d915228
Shwarcz86 <69677756+Shwarcz86@users.noreply.github.com>
2021-07-03 12:53:23 +0300

    Add files via upload

--- Magic/js/main.js
+++ Magic/js/main.js
@@ -14,6 +14,21 @@ $(document).ready(function() {
     $('.top-header').removeClass('_active');
   });
 
+  //Выезжающее меню
+  $(window).scroll(function() {
+    const scrollMenu = $('.bottom-header').offset().top;
+    const scrolled = $(this).scrollTop();
+      if (scrolled > 128) {
+        $('.bottom-header').addClass('active');
+        if (window.matchMedia('(min-width: 1201px)').matches) {
+          $('.page').addClass('scroll');
+        }
+      } else if (scrolled < 128) {
+        $('.bottom-header').removeClass('active');
+        $('.page').removeClass('scroll');
+      }
+  });
+
     //ТЕЛЕФОН МАСКА
     $(function() {
       $("[type=tel]").mask("+7 (999) 999-99-99");
@@ -28,8 +43,9 @@ $(document).ready(function() {
   });
 
   //Выпадания списка в каталоге
-  $('.catalog-page__sidebar-category').on('click', function() {
+  $('.products-page__sidebar-category').on('click', function() {
     $(this).next().slideToggle('slow');
+    $(this).toggleClass('_active');
   });
   
   //Кнопка вверх
@@ -104,57 +120,6 @@ $(document).ready(function() {
 
 
 /*
-
-  //MENU
-  $('.header__btn').on('click', function(e) { 
-    $('.header__btn').toggleClass('header__btn_active');
-    $('.header__menu').toggleClass('header__menu_active');
-    $('body').toggleClass('no-scroll');
-    $('.top-btn').toggleClass('top-btn_disabled');
-  });
-
-  //ТЕЛЕФОН МАСКА
-  $(function() {
-    $("[type=tel]").mask("+7 (999) 999-99-99");
-  });
-
-    //Кнопка вверх
-  var button = $('.top-btn');	
-  $(window).scroll (function () {
-    if ($(this).scrollTop () > 600) {
-      button.fadeIn();
-    } else {
-      button.fadeOut();
-    }
-  });	 
-  button.on('click', function() {
-  $('body, html').animate({
-  scrollTop: 0
-  }, 800);
-  return false;
-  });		 
-
-    //Слайдер
-  $('.brand-page__slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    dots: true,
-    speed: 1500,
-    infinite: false,
-    prevArrow: '<button class="brand-page__slider-btn brand-page__slider-btnprev"><img src="img/arrow-prev.png" alt=""></button>',
-    nextArrow: '<button class="brand-page__slider-btn brand-page__slider-btnnext"><img src="img/arrow-next.png" alt=""></button>',
-    responsive: [
-      {
-        breakpoint: 1421,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          dots: true,
-        }
-      },
-    ]
-  });
-
     //ТАБЫ
   $('.tab').on('click', function(e) { 
     e.preventDefault();
@@ -167,14 +132,6 @@ $(document).ready(function() {
 
     if (window.matchMedia('(max-width: 580px)').matches) { // аналог media-queries
       $('html, body').animate({scrollTop: $($(this).attr('href')).offset().top - 85 +'px'});
-    }
-  });
-
-  //Выпадания списка в меню
-  $('.header__menu-link').on('click', function() {
-    if (window.matchMedia('(max-width: 940px)').matches) {
-      $(this).next().slideToggle('slow');
-      $(this).toggleClass('active');
     }
   });
 
