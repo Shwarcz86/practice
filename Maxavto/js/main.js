07a323b3a7b3c80df62508bbfdc937f0c1dff895
Shwarcz86 <69677756+Shwarcz86@users.noreply.github.com>
2021-07-30 12:08:35 +0300

    Add files via upload

--- Maxavto/js/main.js
+++ Maxavto/js/main.js
@@ -1,5 +1,99 @@
 $(document).ready(function() {
+  
+  //ТЕЛЕФОН МАСКА
+  $(function() {
+    $("[type=tel]").mask("+7 (999) 999-99-99");
+  });
 
+  //Выезжающее меню
+  $(window).scroll(function() {
+    const scrollMenu = $('.bottom-header').offset().top;
+    const scrolled = $(this).scrollTop();
+      if (scrolled > 84) {
+        if (window.matchMedia('(min-width: 1130px)').matches) {
+          $('.bottom-header').addClass('_active');
+          $('.page').addClass('_scroll');
+          $('.bottom-header__menu-list').addClass('_active');
+        }
+      } else if (scrolled < 84) {
+        $('.bottom-header').removeClass('_active');
+        $('.page').removeClass('_scroll');
+        $('.bottom-header__menu-list').removeClass('_active');
+      }
+  });
+
+  //menuHamburger
+  $('.menu-toggle').click(function(){
+    $(this).toggleClass('open');
+    $('.bottom-header').toggleClass('_active');
+    $('body').toggleClass('_no-scroll');
+    $('.top-btn').toggleClass('_disabled');
+  })
+
+  $('.bottom-header__navigation-list li a').on('click', function() {
+    $('.menu-toggle').removeClass('open');
+    $('.bottom-header').removeClass('_active');
+    $('body').removeClass('_no-scroll');
+    $('.top-btn').removeClass('_disabled');
+  });
+
+  //FancyBox
+  $('.fancybox').fancybox();
+
+  //Кнопка вверх
+  var button = $('.top-btn');	
+    $(window).scroll (function () {
+      if ($(this).scrollTop () > 600) {
+        button.fadeIn();
+      } else {
+        button.fadeOut();
+      }
+    });	 
+    button.on('click', function() {
+    $('body, html').animate({
+    scrollTop: 0
+    }, 800);
+    return false;
+  });	
+
+  //Слайдер - главная
+  $('.partners-page__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 500,
+    infinite: false,
+    variableWidth: true,
+    prevArrow: '<button class="partners-page__slider-btn partners-page__slider-btnprev"><img src="img/icons/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="partners-page__slider-btn partners-page__slider-btnnext"><img src="img/icons/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1421,
+        settings: {
+          variableWidth: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 1146,
+        settings: {
+          speed: 500,
+          variableWidth: false,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 701,
+        settings: {
+          speed: 500,
+          variableWidth: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
 
 
 });
@@ -15,10 +109,6 @@ $(document).ready(function() {
     $('.top-btn').toggleClass('top-btn_disabled');
   });
 
-  //ТЕЛЕФОН МАСКА
-  $(function() {
-    $("[type=tel]").mask("+7 (999) 999-99-99");
-  });
 
     //Кнопка вверх
   var button = $('.top-btn');	
