04367d06c5a0eee46281bf5172894d3b4954fbf4
Shwarcz86 <69677756+Shwarcz86@users.noreply.github.com>
2021-09-06 13:58:48 +0300

    Add files via upload

--- Windows/js/main.js
+++ Windows/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+  //Выезжающее меню
+  $(window).scroll(function() {
+    const scrollMenu = $('.bottom-header').offset().top;
+    const scrolled = $(this).scrollTop();
+      if (scrolled > 84) {
+        if (window.matchMedia('(min-width: 1151px)').matches) {
+          $('.bottom-header').addClass('active');
+          $('.bottom-header__menu').addClass('active');
+        }
+      } else if (scrolled < 84) {
+        $('.bottom-header').removeClass('active');
+        $('.bottom-header__menu').removeClass('active');
+      }
+  });
+
   //menuHamburger
   $('.menu-toggle').click(function(){
     $(this).toggleClass('open');
@@ -9,14 +24,6 @@ $(document).ready(function() {
     $('.top-header').toggleClass('_active');
   })
 
-  $('.bottom-header__menu li a').on('click', function() {
-    $('.menu-toggle').removeClass('open');
-    $('.bottom-header').removeClass('_active');
-    $('body').removeClass('_no-scroll');
-    $('.top-btn').removeClass('_disabled');
-    $('.top-header').toggleClass('_active');
-  });
-
   //FancyBox
   $(".fancybox").fancybox();
 
@@ -24,6 +31,59 @@ $(document).ready(function() {
   $(function() {
     $("[type=tel]").mask("+7 (999) 999-99-99");
   });
+
+
+  //Кнопка вверх
+  var button = $('.top-btn');	
+  $(window).scroll (function () {
+    if ($(this).scrollTop () > 600) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });	 
+  button.on('click', function() {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+    //Выпадания списка в моб. меню
+    $('.menu-item-has-children > a').on('click', function() {
+      if (window.matchMedia('(max-width: 1150px)').matches) {
+        $(this).next().slideToggle('slow');
+        $(this).toggleClass('_active');
+      }
+    });
+
+    //Выпадания списка в футере
+    $('.top-footer__column-title').on('click', function() {
+      if (window.matchMedia('(max-width: 630px)').matches) {
+        $(this).next().slideToggle('slow');
+        $(this).toggleClass('_active');
+      }
+    });
+
+    if (window.matchMedia('(max-width: 630px)').matches) {
+      $('.catalog-page__items').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        /*
+        variableWidth: true,
+        */
+        speed: 1500,
+        prevArrow: '<button class="catalog-page__items__btn catalog-page__items__btnprev"><img src="img/icons/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="catalog-page__items__btn catalog-page__items__btnnext"><img src="img/icons/arrow-right.svg" alt=""></button>',
+      });
+
+    //Скроллбар
+    $(window).on("load",function() {
+      $(".catalog-page__item").mCustomScrollbar({
+        theme: "dark"
+      });
+    });
+  }
 
 });
 
@@ -40,21 +100,7 @@ $(document).ready(function() {
 
 
 
-    //Кнопка вверх
-  var button = $('.top-btn');	
-  $(window).scroll (function () {
-    if ($(this).scrollTop () > 600) {
-      button.fadeIn();
-    } else {
-      button.fadeOut();
-    }
-  });	 
-  button.on('click', function() {
-  $('body, html').animate({
-  scrollTop: 0
-  }, 800);
-  return false;
-  });		 
+		 
 
     //Слайдер
   $('.brand-page__slider').slick({
