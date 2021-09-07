d274e42ccecf07c41bcf2ed5eca94a0d9ab221cb
Shwarcz86 <69677756+Shwarcz86@users.noreply.github.com>
2021-09-07 12:58:25 +0300

    Add files via upload

--- Windows/js/main.js
+++ Windows/js/main.js
@@ -48,6 +48,23 @@ $(document).ready(function() {
     return false;
   });
 
+  if (window.matchMedia('(max-width: 630px)').matches) {
+    //Скроллбар
+    $(window).on("load",function() {
+      $(".catalog-page__item").mCustomScrollbar({
+        theme: "dark"
+      });
+    });
+  }
+
+  //Обвертка для таблицы в article
+  $('.selection-item__info article table').wrap('<div class="some-class"></div>');
+
+});
+
+
+
+
   /*
     //Выпадания списка в моб. меню
     $('.menu-item-has-children > a').on('click', function() {
@@ -63,13 +80,8 @@ $(document).ready(function() {
         $(this).toggleClass('_active');
       }
     });
-*/
-
-
-  var screenWidth = $(window).width();
-  $(window).resize(function() {
-    var currScreeWidth = $(window).width();
-    if (currScreeWidth != screenWidth) {
+    
+    function sliderAdapt() {
       if (window.matchMedia('(max-width: 630px)').matches) {
         $('.catalog-page__items').slick({
           slidesToShow: 1,
@@ -79,12 +91,12 @@ $(document).ready(function() {
           prevArrow: '<button class="catalog-page__items__btn catalog-page__items__btnprev"><img src="img/icons/arrow-left.svg" alt=""></button>',
           nextArrow: '<button class="catalog-page__items__btn catalog-page__items__btnnext"><img src="img/icons/arrow-right.svg" alt=""></button>',
           responsive: [
-              {
-               breakpoint: 631,
-               settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1,
-                  speed: 500
+            {
+              breakpoint: 631,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 500
               }
             },
           ]
@@ -93,19 +105,37 @@ $(document).ready(function() {
         $('.catalog-page__items').slick('unslick');
       }
     }
-    screenWidth = $(window).width();
-  }); 
-
-  if (window.matchMedia('(max-width: 630px)').matches) {
-    //Скроллбар
-    $(window).on("load",function() {
-      $(".catalog-page__item").mCustomScrollbar({
-        theme: "dark"
-      });
+    $(document).ready(function(){
+      sliderAdapt();
     });
-  }
-
-  //Обвертка для таблицы в article
-  $('.selection-item__info article table').wrap('<div class="some-class"></div>');
-
-});
+    
+    var screenWidth = $(window).width();
+    $(window).resize(function() {
+      var currScreeWidth = $(window).width();
+      if (currScreeWidth != screenWidth) {
+        if (window.matchMedia('(max-width: 630px)').matches) {
+          $('.catalog-page__items').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            mobileFirst: true,
+            speed: 1500,
+            prevArrow: '<button class="catalog-page__items__btn catalog-page__items__btnprev"><img src="img/icons/arrow-left.svg" alt=""></button>',
+            nextArrow: '<button class="catalog-page__items__btn catalog-page__items__btnnext"><img src="img/icons/arrow-right.svg" alt=""></button>',
+            responsive: [
+              {
+                breakpoint: 631,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  speed: 500
+                }
+              },
+            ]
+          });
+        } else if (window.matchMedia('(min-width: 630px)').matches) {
+          $('.catalog-page__items').slick('unslick');
+        }
+      }
+      screenWidth = $(window).width();
+    }); 
+*/
